403b8ea3e1e7ac065096f7ce575f7f7c8d9c31ad
vizartmd <vizart.md@gmail.com>
2021-07-25 13:02:55 +0300

    Delete UserRepository.java

--- enhanced-task-manager/src/main/java/com/stefanini/enhancedtaskmanager/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.stefanini.enhancedtaskmanager;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
